6f9efb5ad753f2b495bbc7f55768aac963958ba3
Muosvr <lujason2015@gmail.com>
2019-09-01 19:32:43 -0400

    gildedRose tests

--- GildedRose/python/test_gilded_rose.py
+++ GildedRose/python/test_gilded_rose.py
@@ -1,13 +1,72 @@
 # -*- coding: utf-8 -*-
 import unittest
 from gilded_rose import Item, update_quality
+testCases = [
+    {
+        "name": "foo",
+        "sellin": 6,
+        "quality": 10,
+        "results": {
+            5: "foo, 1, 5",
+            7: "foo, -1, 2",
+            8: "foo, -2, 0",
+            10: "foo, -4, 0"
+        }
+    }, {
+        "name": "Aged Brie",
+        "sellin": 5,
+        "quality": 12,
+        "results": {
+            1: "Aged Brie, 4, 13",
+            3: "Aged Brie, 2, 15",
+            7: "Aged Brie, -2, 21",
+            10: "Aged Brie, -5, 27"
+
+        }
+    }, {
+        "name": "Aged Brie",
+        "sellin": 5,
+        "quality": 48,
+        "results": {
+            3: "Aged Brie, 2, 50"
+
+        }
+    }, {
+        "name": "Sulfuras, Hand of Ragnaros",
+        "sellin": 5,
+        "quality": 20,
+        "results": {
+            1: "Sulfuras, Hand of Ragnaros, 5, 20",
+            5: "Sulfuras, Hand of Ragnaros, 5, 20"
+        }
+    }, {
+        "name": "Backstage passes to a TAFKAL80ETC concert",
+        "sellin": 15,
+        "quality": 10,
+        "results": {
+            1: "Backstage passes to a TAFKAL80ETC concert, 14, 11",
+            4: "Backstage passes to a TAFKAL80ETC concert, 11, 14",
+            7: "Backstage passes to a TAFKAL80ETC concert, 8, 19",
+            10: "Backstage passes to a TAFKAL80ETC concert, 5, 25",
+            13: "Backstage passes to a TAFKAL80ETC concert, 2, 34",
+            14: "Backstage passes to a TAFKAL80ETC concert, 1, 37",
+            15: "Backstage passes to a TAFKAL80ETC concert, 0, 40",
+            17: "Backstage passes to a TAFKAL80ETC concert, -2, 0"
+        }
+    }
+]
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        update_quality(items)
-        self.assertEquals("fixme", items[0].name)
+    def testRegular(self):
+        items = [Item(x['name'], x['sellin'], x['quality']) for x in testCases]
+        for dayIndex in range(20):
+            update_quality(items)
+            for index, item in enumerate(testCases):
+                result = item["results"].get(dayIndex + 1)
+                if result:
+                    # print("testing", items[index])
+                    self.assertEquals(result, str(items[index]))
 
 
 if __name__ == "__main__":
--- Tennis/python/tennis.py
+++ Tennis/python/tennis.py
@@ -25,10 +25,8 @@ class TennisGameDefactored1:
             3: "Forty",
         }
         if (self.p1points == self.p2points):
-            result = scoreTypes.get(self.p1points, "")
-            if not result:
-                result = "Deuce"
-            else:
+            result = scoreTypes.get(self.p1points, "Deuce")
+            if result != "Deuce":
                 result += "-All"
 
         elif max(self.p1points, self.p2points) >= 4:
@@ -63,59 +61,74 @@ class TennisGameDefactored2:
 
     def score(self):
         result = ""
-        scoreTypes = {
-            0: "Love",
-            1: "Fifteen",
-            2: "Thirty",
-            3: "Forty"
-        }
-        scoreDifference = abs(self.p1points - self.p2points)
-        maxScore = max(self.p1points, self.p2points)
-        play1Leads = True if self.p1points > self.p2points else False
+        scoreTypes = ["Love", "Fifteen", "Thirty", "Forty"]
 
         if self.p1points == self.p2points:
             if self.p1points < 4:
                 result = scoreTypes[self.p1points] + "-All"
             else:
                 result = "Deuce"
-        elif maxScore <= 3:
+        elif max(self.p1points, self.p2points) <= 3:
             P1res = scoreTypes[self.p1points]
             P2res = scoreTypes[self.p2points]
             result = P1res + "-" + P2res
         else:
-            if scoreDifference == 1:
+            if abs(self.p1points - self.p2points) == 1:
                 result = "Advantage "
             else:
                 result = "Win for "
-            result += self.player1Name if play1Leads else self.player2Name
+
+            if self.p1points > self.p2points:
+                result += self.player1Name
+            else:
+                result += self.player2Name
 
         return result
 
 
 class TennisGameDefactored3:
     def __init__(self, player1Name, player2Name):
-        self.p1N = player1Name
-        self.p2N = player2Name
-        self.p1 = 0
-        self.p2 = 0
+        self.p1Name = player1Name
+        self.p2Name = player2Name
+        self.p1Points = 0
+        self.p2Points = 0
 
     def won_point(self, n):
-        if n == self.p1N:
-            self.p1 += 1
+        if n == self.p1Name:
+            self.p1Points += 1
         else:
-            self.p2 += 1
+            self.p2Points += 1
 
     def score(self):
-        if (self.p1 < 4 and self.p2 < 4):
-            p = ["Love", "Fifteen", "Thirty", "Forty"]
-            score = p[self.p1]
-            return score + "-All" if (self.p1 == self.p2) else score + "-" + p[self.p2]
+        result = ""
+        score = ""
+        if max(self.p1Points, self.p2Points) <= 3:
+            scoreTypes = ["Love", "Fifteen", "Thirty", "Forty"]
+            p1Score = scoreTypes[self.p1Points]
+            p2Score = scoreTypes[self.p2Points]
+
+            if self.p1Points == self.p2Points:
+                result = p1Score + "-All"
+            else:
+
+                result = p1Score + "-" + p2Score
+
         else:
-            if (self.p1 == self.p2):
-                return "Deuce"
-            score = self.p1N if self.p1 > self.p2 else self.p2N
-            return "Advantage " + score if ((self.p1-self.p2)*(self.p1-self.p2) == 1) else "Win for " + score
+            if (self.p1Points == self.p2Points):
+                result = "Deuce"
+            else:
+                winner = ""
+                if self.p1Points > self.p2Points:
+                    winner = self.p1Name
+                else:
+                    winner = self.p2Name
+
+                if abs(self.p1Points-self.p2Points) == 1:
+                    result = "Advantage " + winner
+                else:
+                    result = "Win for " + winner
+        return result
 
 
 # NOTE: You must change this to point at the one of the three examples that you're working on!
-TennisGame = TennisGameDefactored3
+TennisGame = TennisGameDefactored1
